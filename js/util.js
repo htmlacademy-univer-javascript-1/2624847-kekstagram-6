b24fd0153891bb3cf2504bad18912bd68516eaf6
elinaaa009 <orbelyan_elina0@mail.ru>
2025-10-13 14:50:08 +0500

    Update util.js

--- js/util.js
+++ js/util.js
@@ -2,4 +2,4 @@ const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-export {getRandomArrayElement, getRandomInteger};
+export {getRandomArrayElement};
