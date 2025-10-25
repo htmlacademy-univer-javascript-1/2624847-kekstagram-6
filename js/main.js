50c63a4b068716b55431f0e2a02f768aeb9131dd
elinaaa009 <orbelyan_elina0@mail.ru>
2025-10-25 15:15:18 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
-import './util.js';
-import './data.js';
+import {renderThumbnails} from './thumbnail.js';
+import {generatePhotosArray} from './data.js';
+
+renderThumbnails(generatePhotosArray());
