07b575da2d2e8f842fd4e48c4f0112fd3419396e
Keks <keks@htmlacademy.ru>
2025-12-18 14:45:43 +0300

    :heavy_check_mark: Сборка #22

---
+++ 22/js/filters.js
@@ -0,0 +1,79 @@
+import { getRandomInteger } from './util.js';
+
+const RANDOM_PHOTOS_COUNT = 10;
+
+let selectedFilter = 'filter-default';
+let photos = [];
+let filterBtns;
+
+const sortingCommentsCount = (photoA, photoB) => photoB.comments.length - photoA.comments.length;
+
+const filteringPhotos = (photosArray) => {
+  let photosForRendering = [];
+  let temporaryStorage = [];
+
+  switch (selectedFilter) {
+    case 'filter-discussed':
+      photosForRendering = photosArray.slice().sort(sortingCommentsCount);
+      break;
+
+    case 'filter-random':
+      temporaryStorage = photosArray.slice();
+      for (let i = 0; i < RANDOM_PHOTOS_COUNT && temporaryStorage.length > 0; i++) {
+        const randomPhotoIndex = getRandomInteger(0, temporaryStorage.length - 1);
+        photosForRendering.push(temporaryStorage[randomPhotoIndex]);
+        temporaryStorage.splice(randomPhotoIndex, 1);
+      }
+      break;
+
+    default:
+      photosForRendering = photosArray;
+      break;
+  }
+
+  return photosForRendering;
+};
+
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+const filterBtnsAddEvent = (callback) => {
+  filterBtns = document.querySelectorAll('.img-filters__button');
+
+  filterBtns.forEach((filterBtn) => {
+    filterBtn.addEventListener('click', (evt) => {
+      selectedFilter = evt.target.id;
+
+      filterBtns.forEach((button) => {
+        button.classList.remove('img-filters__button--active');
+      });
+
+      evt.target.classList.add('img-filters__button--active');
+
+      callback();
+    });
+  });
+};
+
+const initFilters = (loadedPhotos, renderCallback) => {
+  photos = loadedPhotos;
+
+  const filtersContainer = document.querySelector('.img-filters');
+  if (filtersContainer) {
+    filtersContainer.classList.remove('img-filters--inactive');
+  }
+
+  filterBtnsAddEvent(debounce(() => {
+    const filteredPhotos = filteringPhotos(photos);
+    renderCallback(filteredPhotos);
+  }));
+
+  return filteringPhotos(photos);
+};
+
+export { initFilters };
---
+++ 22/js/main.js
@@ -0,0 +1,51 @@
+import './hashtags-pristine.js';
+import { renderThumbnails } from './thumbnail.js';
+import { openBigPicture } from './big-picture.js';
+import { getDataFromServer } from './api.js';
+import { initFilters } from './filters.js';
+
+let photos = [];
+
+const updatePhotos = (filteredPhotos) => {
+  const picturesContainer = document.querySelector('.pictures');
+  const oldPictures = picturesContainer.querySelectorAll('.picture');
+  oldPictures.forEach((picture) => picture.remove());
+
+  renderThumbnails(filteredPhotos);
+
+  const thumbnails = document.querySelectorAll('.picture');
+  thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+      openBigPicture(filteredPhotos[index]);
+    });
+  });
+};
+
+const onSuccessLoad = (loadedPhotos) => {
+  photos = loadedPhotos;
+
+  const filteredPhotos = initFilters(photos, updatePhotos);
+
+  updatePhotos(filteredPhotos);
+};
+
+const onErrorLoad = () => {
+  const errorBlock = document.createElement('div');
+  errorBlock.style.cssText = `
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    padding: 15px;
+    background: red;
+    color: white;
+    text-align: center;
+    z-index: 1000;
+  `;
+  errorBlock.textContent = 'Ошибка загрузки данных';
+  document.body.appendChild(errorBlock);
+};
+
+getDataFromServer(onSuccessLoad, onErrorLoad);
+
+
